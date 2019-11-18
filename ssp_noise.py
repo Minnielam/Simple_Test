3aa1a25d91ef2291969b3d4c48e3d5c81c500c42
Minnielam <47394920+Minnielam@users.noreply.github.com>
2019-11-18 13:51:00 +0100

    Add files via upload

--- ssp_noise.py
+++ ssp_noise.py
@@ -1,6 +1,9 @@
 #################
 # This is the function of applying redshift and applying noise to a noiseless, rest-frame data.
 
+# 2019-11-18: fixed the bug of redshift created. Change the IO package from Paradise to Astropy
+
+
 # Requirements:
 # numpy
 # astropy
@@ -13,7 +16,6 @@
 import numpy as np
 import astropy.io.fits as pyfits
 from astropy.io import fits
-import Paradise
 from matplotlib import pyplot as plt
 import math
 
@@ -65,17 +67,12 @@ def apply_noise(data = None, noise_level = None):
     (y,x) = data.shape
 
     for length in range(y):
-        data_mean = np.mean(data[length,:])
-        sig_data = 10 * np.log10(data_mean)
-
-        noise_level_new = sig_data - noise_level
-        noise_new = 10**(noise_level_new/10) 
 
         mean_noise = 0
-        noise = np.random.normal(mean_noise, np.sqrt(noise_new), len(data[length,:]))
+        noise = np.random.normal(mean_noise, data[length,:]/noise_level, len(data[length,:]))
 
         new_noise_data.append(data[length,:] + noise)
-        new_error_data.append(noise)
+        new_error_data.append(data[length,:]/noise_level)
 
 
     return new_noise_data, new_error_data
@@ -94,41 +91,72 @@ PATH = '/Volumes/Transcend/pyparadise/simulation_spectra/test_noiseless/test_CB0
 pre='test_CB09_spectra'
 prefix = '_noiseless'
 
-rss = Paradise.loadRSS(PATH+pre+prefix+'.fits')
+filename = fits.open(PATH+pre+prefix+'.fits')
+rss = filename[0].data
+header = filename[0].header
 
-snr = 40.0 
+snr = 50.0 
 
 data_with_noise = []
 
-(y,x) = rss._data.shape
+(y,x) = rss.shape
 
-data_with_noise, data_error = apply_noise(rss._data, snr)
+data_with_noise, data_error = apply_noise(rss, snr)
 
 print(np.array(data_with_noise).shape)
 
-
-plt.figure()
-plt.plot(rss._wave,np.array(data_with_noise)[28])
-plt.plot(rss._wave, rss._data[28])
-plt.show()
 
 
 
 """
 
-Create the new fits file
+Adding redshift to the noise data
+
+"""
+
+z = 0.01
+
+wave = header['CRVAL1'] + header['CDELT1'] * np.arange(header['NAXIS1'])
+
+new_wave = apply_redshift(wave, z)
+
+new_fwhm = float(header['SPECFWHM']) * (1+z)
+
+print(new_fwhm)
+
+
+
+plt.figure()
+plt.plot(wave, rss[28])
+plt.plot(new_wave, data_with_noise[28])
+plt.show()
+
+
+
+
+
+
+
+
+"""
+
+Create the new fits file with redshift
+
 
 """
 
 hdus = [None, None]
 hdus[0] = pyfits.PrimaryHDU()
 hdus[1] = pyfits.ImageHDU()
-hdus[0].header = rss._header
+hdus[0].header = header
+hdus[0].header['CRVAL1'] = new_wave[0]
+hdus[0].header['CDELT1'] =  header['CDELT1'] * (1+z)
+hdus[0].header['SPECFWHM'] = new_fwhm
 hdus[1].header['EXTNAME'] = 'ERROR'
 hdus[0].data = data_with_noise
 hdus[1].data = data_error
 
 hdu = pyfits.HDUList(hdus)
-hdu.writeto(PATH+pre+'_noise'+'.fits', overwrite=True)
+hdu.writeto(PATH+pre+'_noise50_z001_new'+'.fits', overwrite=True)
 
 
